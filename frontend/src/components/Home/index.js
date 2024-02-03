cb8c39dc9c2e1181b769196b1e4a973df0514687
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2024-02-03 14:56:48 -0500

    Made the backend server

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,40 +1,55 @@
 //import { Link } from 'react-router-dom'
 import './index.scss'
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
-import Logo from './Icon';
-
+import Logo from './Icon'
+import axios from 'axios'
 
 export const Home = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const healthArray = ['h', 'e', 'a', 'l', 't', 'h', ' ']
-    const youArray = [' ', 'you']
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const healthArray = ['h', 'e', 'a', 'l', 't', 'h', ' ']
+  const youArray = [' ', 'you']
+  const [backendOutput, setBackendOutput] = useState('')
 
-    useEffect(() => {
-        setTimeout(() => {
-          setLetterClass('text-animate-hover')
-        }, 400)
-      }, [])
+  useEffect(() => {
+    fetchBackendData()
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 400)
+  }, [])
 
-    return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                <AnimatedLetters letterClass={letterClass}
-                    strArray={healthArray}
-                    idx={15} />
-                    and  
-                    <AnimatedLetters letterClass={letterClass}
-                    strArray={youArray}
-                    idx={15} />
-                </h1>
-                <h2>Tech</h2>
-            </div>
+  const fetchBackendData = async () => {
+    try {
+      const response = await axios.post('http://localhost:8000/api/algorithm') // Adjust endpoint URL and changed from axios.get to axios.post
+      setBackendOutput(response.data.result) // Changed from response.data to response.data.result
+    } catch (error) {
+      console.error('Error fetching backend data:', error)
+      // Handle errors gracefully, e.g., display an error message
+    }
+  }
 
-            <Logo />
-        </div>
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={healthArray}
+            idx={15}
+          />
+          and
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={youArray}
+            idx={15}
+          />
+        </h1>
+        <h2>Tech {backendOutput} </h2>
+      </div>
 
-    );
+      <Logo />
+    </div>
+  )
 }
 
 export default Home
