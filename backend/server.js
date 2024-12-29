c91f57e56be8cfbb615fb24b9be85291f82b4dd5
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2024-12-29 16:56:46 -0500

    refactor: Refactor CORS middleware in server.js

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,13 @@ const app = express();
 const port = 8000;
 
 // Middlewares
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 app.use(bodyParser.json());
 app.use("/api/users", userRoutes, periodCycleRoutes);
 app.use("/api/data", periodCycleRoutes);
