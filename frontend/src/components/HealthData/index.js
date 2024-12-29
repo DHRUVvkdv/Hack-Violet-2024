4d572df41ebe362bca049edbe4a64a1d5e9921c9
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2024-12-29 12:36:42 -0500

    refactor: corrected the TopNav page to make it work with other files

--- frontend/src/components/HealthData/index.js
+++ frontend/src/components/HealthData/index.js
@@ -26,7 +26,7 @@ const MenstrualCyclePage = () => {
     Ovulation: 'During the ovulatory phase, estrogen and testosterone levels peak, providing a boost in strength and endurance. Take advantage of this by incorporating more demanding strength training exercises. Challenge yourself with heavier weights and lower rep ranges to promote muscle hypertrophy. Compound movements and powerlifting exercises, such as deadlifts, squats, and overhead presses, can be particularly effective during this phase. Focus on maintaining proper form and control throughout each repetition.',
     Luteal: 'As you enter the luteal phase, you may experience increased PMS symptoms and fatigue. Modify your strength training routine to prioritize stability and control. Consider incorporating more bodyweight exercises, resistance band workouts, and isometric holds. This phase is suitable for targeting specific muscle groups with isolation exercises and incorporating yoga or Pilates movements to promote flexibility and alleviate tension. Be attentive to your bodys signals, and if needed, decrease the overall training volume while maintaining a focus on quality movement.',
   }
-  
+
   const yogaData = {
     Menstruation: 'Focus: During your period, the body is often fatigued and may experience discomfort. The emphasis is on self-care and gentleness. Yoga Practice: Opt for restorative yoga, which involves gentle, supported poses to promote relaxation. Incorporate slow, mindful movements to ease tension. Poses to Try: Child Pose, Supported Bridge Pose with props, Legs Up the Wall, and gentle seated forward bends.',
     Follicular: 'Focus: Energy levels rise as the body prepares for ovulation. The focus here is on building strength, flexibility, and enhancing mood. Yoga Practice: Engage in more dynamic and invigorating sequences. Include sun salutations, standing poses like Warrior series, and flowing movements to boost vitality. Poses to Try: Sun Salutations, Warrior Poses, Downward Dog, and inversions like Headstand or Handstand (if you are comfortable with them).',
@@ -40,7 +40,7 @@ const MenstrualCyclePage = () => {
     Ovulation: 'During ovulation, take advantage of increased endurance and stamina. Include cardio workouts that elevate the heart rate and promote cardiovascular health. Activities like running, cycling, and group fitness classes can be effective. Experiment with longer duration and varied intensities to enhance overall endurance.',
     Luteal: 'As you enter the luteal phase, be mindful of potential fatigue and mood changes. Opt for lower-impact cardio exercises such as brisk walking, cycling, or elliptical training. Focus on maintaining a steady pace to support cardiovascular health without placing excessive stress on the body. Listen to your bodys cues and choose activities that feel nurturing.',
   };
-  
+
 
   const foodData = {
     Veg: 'Information about vegetarian food...',
@@ -51,9 +51,9 @@ const MenstrualCyclePage = () => {
   const generalFoodData = {
     Menstruation:
       'During your period, focus on replenishing iron and energy levels. Include foods rich in iron, such as lean meats, fish, spinach, and legumes. Hydration is crucial, so drink plenty of water and consider herbal teas. Incorporate foods high in omega-3 fatty acids, like flaxseeds and walnuts, to help reduce inflammation and support mood. Opt for warm, comforting foods like soups and stews, and consider incorporating magnesium-rich foods, such as dark chocolate and bananas, to ease muscle cramps.',
-      Follicular: 'As energy levels rise, focus on a balanced diet that supports increased activity. Include lean proteins, whole grains, and a variety of colorful fruits and vegetables. Choose complex carbohydrates for sustained energy and incorporate foods rich in vitamin B, such as poultry, eggs, and leafy greens, to support the nervous system. Stay hydrated, and consider adding a variety of seeds, like chia and pumpkin seeds, for additional nutrients.',
-      Ovulation: 'With increased energy and metabolic rate, emphasize nutrient-dense foods. Include lean proteins, such as fish and poultry, for muscle support. Incorporate complex carbohydrates, like quinoa and sweet potatoes, for sustained energy. Boost your intake of antioxidants from colorful fruits and vegetables. Include foods high in zinc, such as nuts and seeds, to support immune function. Stay hydrated with water and herbal teas, and consider incorporating probiotic-rich foods like yogurt for gut health.',
-      Luteal: 'During the luteal phase, address potential cravings and mood swings with a balanced and supportive diet. Include complex carbohydrates, such as whole grains and legumes, to regulate blood sugar levels and alleviate PMS symptoms. Consume foods rich in calcium, like dairy or fortified plant-based alternatives, to support bone health. Incorporate magnesium-rich foods, such as leafy greens and almonds, to help with muscle relaxation. Stay hydrated and consider herbal teas for additional comfort. Limit caffeine and refined sugars to help manage mood swings and energy levels.',
+    Follicular: 'As energy levels rise, focus on a balanced diet that supports increased activity. Include lean proteins, whole grains, and a variety of colorful fruits and vegetables. Choose complex carbohydrates for sustained energy and incorporate foods rich in vitamin B, such as poultry, eggs, and leafy greens, to support the nervous system. Stay hydrated, and consider adding a variety of seeds, like chia and pumpkin seeds, for additional nutrients.',
+    Ovulation: 'With increased energy and metabolic rate, emphasize nutrient-dense foods. Include lean proteins, such as fish and poultry, for muscle support. Incorporate complex carbohydrates, like quinoa and sweet potatoes, for sustained energy. Boost your intake of antioxidants from colorful fruits and vegetables. Include foods high in zinc, such as nuts and seeds, to support immune function. Stay hydrated with water and herbal teas, and consider incorporating probiotic-rich foods like yogurt for gut health.',
+    Luteal: 'During the luteal phase, address potential cravings and mood swings with a balanced and supportive diet. Include complex carbohydrates, such as whole grains and legumes, to regulate blood sugar levels and alleviate PMS symptoms. Consume foods rich in calcium, like dairy or fortified plant-based alternatives, to support bone health. Incorporate magnesium-rich foods, such as leafy greens and almonds, to help with muscle relaxation. Stay hydrated and consider herbal teas for additional comfort. Limit caffeine and refined sugars to help manage mood swings and energy levels.',
   }
 
   const mealFoodData = {
@@ -90,7 +90,7 @@ const MenstrualCyclePage = () => {
       - Hydration: Herbal teas like chamomile or ginger, and water with a slice of lemon
     `,
   };
-  
+
 
   const veganFoodData = {
     Menstruation: `
@@ -106,7 +106,7 @@ const MenstrualCyclePage = () => {
       During the luteal phase, address potential cravings and mood swings with a balanced and nourishing vegan diet. Begin your day with whole grain toast topped with avocado and tomato. Snack on a bowl of mixed nuts and dried fruits for sustained energy. For lunch, try a lentil and sweet potato curry served over brown rice. Include calcium-rich plant-based sources like fortified almond milk and tofu in your meals. Hydrate with herbal teas like chamomile and ginger, and limit caffeine and refined sugars to manage mood swings and energy levels.
     `,
   };
-  
+
   const handlePhaseClick = (phase) => {
     setSelectedPhase(phase)
   }
